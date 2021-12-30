fa9cfb27789b4c23f0bd025c708b89fb8ca84b58
Tasneem Abdullah <12tasnimtasnim@gmail.com>
2021-12-30 12:55:14 +0600

    All functionality done...

--- BMICalculator/app/src/main/java/com/tasneem/bmicalculator/MainActivity.java
+++ BMICalculator/app/src/main/java/com/tasneem/bmicalculator/MainActivity.java
@@ -2,7 +2,11 @@ package com.tasneem.bmicalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -23,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     int user_feet,user_inchi, user_age;
     float  user_weight;
     Button calcBtn;
+    TextView tvDes, tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,10 +46,9 @@ public class MainActivity extends AppCompatActivity {
         inchi_value = findViewById(R.id.inchi_value);
         weight_value = findViewById(R.id.weight_value);
         calcBtn = findViewById(R.id.calcBtn);
-        user_feet = 0;
-        user_weight = Integer.parseInt(weight_value.getText().toString());
-        user_inchi = 0;
         user_age = 0;
+        tvDes = findViewById(R.id.tvDes);
+        tvResult = findViewById(R.id.tvResult);
 
 
         btnMale.setOnClickListener(new View.OnClickListener() {
@@ -139,6 +143,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        kg_plus.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                if (0 < 500){
+                    user_weight+= 10;
+                    weight_value.setText(""+user_weight);
+                }
+                return true;
+            }
+        });
+
         kg_minus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -154,6 +169,46 @@ public class MainActivity extends AppCompatActivity {
         calcBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                    user_feet = Integer.parseInt(feet_value.getText().toString());
+                    user_weight = Float.parseFloat(weight_value.getText().toString());
+                    user_inchi = Integer.parseInt(inchi_value.getText().toString());
+                    float height = (float) ((user_feet * 0.3048) + (user_inchi * 0.0254));
+                    float bmiIndex = 0;
+
+                    if(height != 0 && user_weight !=0){
+
+                        bmiIndex = user_weight /(height*height);
+                        tvResult.setText("YOUR BMI INDEX IS: " + String.format("%.2f", bmiIndex));
+                        tvResult.setVisibility(View.VISIBLE);
+                        tvDes.setVisibility(View.VISIBLE);
+                    }
+                    else{
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setMessage("Please Enter Values First..");
+                        builder.setNeutralButton("Ok",new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        });
+                        builder.setCancelable(true);
+
+                        AlertDialog alertDialog = builder.create();
+                        alertDialog.show();
+                    }
+
+                    if (bmiIndex < 18.5){
+                        tvDes.setText("You're underweight. Please eat healthy and nutritious food to increase your BMI Index.");
+                    }
+                    else if(bmiIndex >= 18.5 && bmiIndex <= 24.9){
+                        tvDes.setText("Your BMI Index is perfect. Keep it up. \n 😎 😎");
+                    }
+                    else if (bmiIndex >= 25 && bmiIndex <= 29.9){
+                        tvDes.setText("Your BMI Index is overweight. You should control your diet and exercise regularly.");
+                    }
+                    else{
+                        tvDes.setText("You're too overweight. You should control your diet and exercise regularly. Please control your weight... ");
+                    }
 
             }
         });
